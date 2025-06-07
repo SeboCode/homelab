da41e706178136ff912f23ef58be27320ff06107
SeboCode <git@mikemarti.ch>
2025-06-07 17:21:04 +0200

    Split environment dependent variables into separate variable files

--- dev.sh
+++ dev.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-ansible-playbook deploy-dev.yaml -i inventory/dev.ini
+ansible-playbook deploy-dev.yaml -i inventory/dev.ini -e "@vars/dev.yaml"
--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-ansible-playbook deploy-test.yaml -i inventory/test.ini
+ansible-playbook deploy-test.yaml -i inventory/test.ini -e "@vars/test.yaml"
