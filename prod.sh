9bb23243e111c1c781c88470c80fa55bed103b77
SeboCode <git@mikemarti.ch>
2025-08-02 00:57:06 +0200

    Add ssh agent and agent population to prod bash script as well as trap cleanup steps to remove the private key and agent after installation

--- prod.sh
+++ prod.sh
@@ -1,3 +1,17 @@
 #!/usr/bin/env bash
 
+echo "Starting and configuring ssh-agent to populate ssh private key for later use by Ansible..."
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/homelab
+
+cleanup() {
+    echo "Cleaning up ssh-agent and ssh private key..."
+    ssh-add -d ~/.ssh/homelab
+    eval "$(ssh-agent -k)"
+}
+
+trap cleanup EXIT
+trap cleanup INT
+trap cleanup TERM
+
 ansible-playbook deploy.yaml -i inventory/prod.ini -e "@vars/prod.yaml"
