0da6533e7563c6c3cab6c8fe7e7b9011398bf58e
SeboCode <git@mikemarti.ch>
2025-08-18 01:40:45 +0200

    Add local development tls setting to traefik configuration

---
+++ roles/traefik/files/generate.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+openssl req -x509 -nodes -days 365 \
+  -newkey rsa:2048 \
+  -keyout certs/server.key \
+  -out certs/server.crt \
+  -subj "/CN=*.localhost" \
+  -addext "subjectAltName = DNS:immich.localhost,DNS:nextcloud.localhost,DNS:photoprism.localhost"
+
