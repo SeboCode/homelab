d62699c43498d7282c2cc452bf3acc262b219498
SeboCode <git@mikemarti.ch>
2025-11-27 23:06:19 +0100

    Add zfs installation, pool creation and configuration

--- dev.sh
+++ dev.sh
@@ -11,6 +11,8 @@ source virtualenv.sh
 cd "./platform-stack/$platformstack"
 
 vagrant destroy -f
+sudo virsh vol-delete --pool default  server_dev-vdb.qcow2
+sudo virsh vol-delete --pool default  server_dev-vdc.qcow2
 vagrant up --no-provision
 vagrant provision --provision-with vm-setup
 vagrant reload --no-provision
