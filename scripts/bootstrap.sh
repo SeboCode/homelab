82749737569591e5e4f40d3da6ab60c2969c685f
SeboCode <git@mikemarti.ch>
2026-06-06 02:52:03 +0200

    mise: Move mise into dependencies subdirectory whilst maintaining the environment variables

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -8,6 +8,6 @@ dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 require_command="${dir}/require-command.sh"
 mise=$(${require_command} mise)
 
-cd "${dir}/../"
+cd "${dir}/../dependencies/mise/"
 ${mise} install
 
