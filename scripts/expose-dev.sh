4bbf2da98177f6ae12c711bfdc0847ef37f2a391
SeboCode <git@mikemarti.ch>
2026-06-24 20:49:07 +0200

    bug: Fix faulty variable name in script

--- scripts/expose-dev.sh
+++ scripts/expose-dev.sh
@@ -6,7 +6,7 @@ set -o pipefail
 
 dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 require_command="${dir}/require-command.sh"
-ansible-playbook=$(${require_command} ansible-playbook)
+ansible_playbook=$(${require_command} ansible-playbook)
 
-${ansible-playbook} --ask-become-pass "$dir/../deploy/ansible/expose.yaml"
+${ansible_playbook} --ask-become-pass "$dir/../deploy/ansible/expose.yaml"
 
