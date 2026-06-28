e68f1d2adeef809cdaa67d1ceb7e9e86c9cf4c36
SeboCode <git@mikemarti.ch>
2026-06-28 12:16:57 +0200

    argocd: Use server-side apply when provisioning the test cluster and separate the root application apply call from kustomize to avoid a race condition in the availability of the argocd crds

--- scripts/cluster-provision-test.sh
+++ scripts/cluster-provision-test.sh
@@ -7,9 +7,12 @@ set -o nounset
 dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 require_command="${dir}/require-command.sh"
 kustomize=$(${require_command} kustomize)
+kubectl=$(${require_command} kubectl)
 yq=$(${require_command} yq)
 
 config_file="${dir}/../.direnv/kubeconfig"
 cluster_context="k3d-$(${yq} ".metadata.name" "${dir}/../deploy/k3d/cluster.yaml")"
-${kustomize} build "${dir}/../deploy/argocd/bootstrap" | kubectl --kubeconfig "${config_file}" --context "${cluster_context}" apply -f -
+${kustomize} build "${dir}/../deploy/argocd/bootstrap/argocd" | kubectl --kubeconfig "${config_file}" --context "${cluster_context}" apply --server-side -f -
+${kubectl} wait --for condition=established --timeout=15s crd/applications.argoproj.io
+${kubectl} apply -f "${dir}/../deploy/argocd/bootstrap/root-app.yaml" # cannot be applied with the argocd bootstrap due to a race condition of the argocd apiVersion and kind not existing yet
 
