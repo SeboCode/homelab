4dd9c2988821e1ef89a43feb7c00b467b1e9278e
SeboCode <git@mikemarti.ch>
2026-07-01 23:41:47 +0200

    bug: Fix invalid variable name in script

--- scripts/provision-prod.sh
+++ scripts/provision-prod.sh
@@ -8,9 +8,9 @@ node="${1:-}"
 
 dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 require_command="${dir}/require-command.sh"
-ssh-agent=$(${require_command} ssh-agent)
-ssh-add=$(${require_command} ssh-add)
-ansible-playbook=$(${require_command} ansible-playbook)
+ssh_agent=$(${require_command} ssh-agent)
+ssh_add=$(${require_command} ssh-add)
+ansible_playbook=$(${require_command} ansible-playbook)
 
 case "$node" in
     charon|daisy)
@@ -22,20 +22,20 @@ case "$node" in
 esac
 
 echo "Starting and configuring ssh-agent to populate ssh private key for later use by Ansible..."
-eval "$(${ssh-agent} -s)"
-${ssh-add} ~/.ssh/homelab-$node
+eval "$(${ssh_agent} -s)"
+${ssh_add} ~/.ssh/homelab-$node
 
 cleanup() {
     echo "Cleaning up ssh-agent and ssh private key..."
-    ${ssh-add} -d ~/.ssh/homelab-$node
-    eval "$(${ssh-agent} -k)"
+    ${ssh_add} -d ~/.ssh/homelab-$node
+    eval "$(${ssh_agent} -k)"
 }
 
 trap cleanup EXIT
 trap cleanup INT
 trap cleanup TERM
 
-${ansible-playbook} "$dir/../deploy/ansible/$node.yaml" \
+${ansible_playbook} "$dir/../deploy/ansible/$node.yaml" \
     --ask-vault-pass \
     --inventory="$dir/../deploy/ansible/inventory/$node.enc.ini" \
     --extra-vars="@$dir/../deploy/ansible/host_vars/prod/shared-secrets.enc.yaml" \
