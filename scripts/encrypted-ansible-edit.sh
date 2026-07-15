178bd86640e6c4ba3f46b1f4529f22023e4290f1
SeboCode <git@mikemarti.ch>
2026-07-16 00:37:47 +0200

    infra: Simplify ansible secret management script

--- scripts/encrypted-ansible-edit.sh
+++ scripts/encrypted-ansible-edit.sh
@@ -4,24 +4,11 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-category="$1"
-env="$2"
-filename="$3"
+file="$1"
 
 dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 require_command="${dir}/require-command.sh"
 ansible_vault=$(${require_command} ansible-vault)
 
-case "${category}" in
-    inv)
-        ${ansible_vault} edit "${dir}/../deploy/ansible/inventory/${filename}.enc.yaml"
-        ;;
-    var)
-        ${ansible_vault} edit "${dir}/../deploy/ansible/host_vars/${env}/${filename}.enc.ini"
-        ;;
-    *)
-        echo "Error: Invalid or missing category '${category}'. Valid options: inv, vars" >&2
-        exit 1
-        ;;
-esac
+${ansible_vault} edit "${file}"
 
