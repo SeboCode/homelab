647cdd2e1681edc712d535b1ce1a3c2edbfe0a60
SeboCode <git@mikemarti.ch>
2026-08-21 23:31:34 +0200

    infra: Override editor to vim when editing secret

--- scripts/encrypted-ansible-edit.sh
+++ scripts/encrypted-ansible-edit.sh
@@ -9,6 +9,8 @@ file="$1"
 dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 require_command="${dir}/require-command.sh"
 ansible_vault=$(${require_command} ansible-vault)
+vim=$(${require_command} vim)
 
-${ansible_vault} edit "${file}"
+EDITOR="${vim}" \
+    ${ansible_vault} edit "${file}"
 
--- scripts/encrypted-sops-create.sh
+++ scripts/encrypted-sops-create.sh
@@ -11,8 +11,10 @@ dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 require_command="${dir}/require-command.sh"
 sops=$(${require_command} sops)
 age=$(${require_command} age)
+vim=$(${require_command} vim)
 
 sops_key=$(${age} -d "${dir}/../deploy/kubernetes/sops-${env}-secret-key.enc.age")
-SOPS_AGE_KEY="${sops_key}" \
+EDITOR="${vim}" \
+    SOPS_AGE_KEY="${sops_key}" \
     ${sops} --config "${dir}/../deploy/kubernetes/sops-config.yaml" "${file}"
 
