ffb9ad17b1d22cbd937715bca2c186c525b060e3
SeboCode <git@mikemarti.ch>
2026-08-22 12:37:04 +0200

    argocd: Add traefik, cert-manager and sops-secrets-operator application manifest files

--- scripts/cluster-provision-test.sh
+++ scripts/cluster-provision-test.sh
@@ -9,10 +9,21 @@ require_command="${dir}/require-command.sh"
 kustomize=$(${require_command} kustomize)
 kubectl=$(${require_command} kubectl)
 yq=$(${require_command} yq)
+age=$(${require_command} age)
 
 config_file="${dir}/../.direnv/kubeconfig"
 cluster_context="k3d-$(${yq} ".metadata.name" "${dir}/../deploy/k3d/cluster.yaml")"
-${kustomize} build "${dir}/../deploy/argocd/bootstrap/argocd" | kubectl --kubeconfig "${config_file}" --context "${cluster_context}" apply --server-side -f -
+
+# Load the age key before anything gets applied to the cluster
+sops_key=$(${age} -d "${dir}/../deploy/kubernetes/sops-prod-secret-key.enc.age")
+
+# Bootstrap age key secret and namespace for sops-secrets-operator
+${kubectl} --kubeconfig "${config_file}" --context "${cluster_context}" apply -f "${dir}/../deploy/kubernetes/apps/sops-secrets-operator/manual/namespace.yaml" 
+SOPS_AGE_KEY="${sops_key}" sops -d "${dir}/../deploy/kubernetes/apps/sops-secrets-operator/manual/age-key-secret.dev.enc.yaml" | \
+    ${kubectl} --kubeconfig "${config_file}" --context "${cluster_context}" apply -f -
+
+${kustomize} build "${dir}/../deploy/argocd/bootstrap/argocd" | ${kubectl} --kubeconfig "${config_file}" --context "${cluster_context}" apply --server-side -f -
 ${kubectl} wait --for condition=established --timeout=15s crd/applications.argoproj.io
-${kubectl} apply -f "${dir}/../deploy/argocd/bootstrap/root-app.yaml" # cannot be applied with the argocd bootstrap due to a race condition of the argocd apiVersion and kind not existing yet
+# cannot be applied with the argocd bootstrap due to a race condition of the argocd application crd not existing yet (hence the wait)
+${kubectl} --kubeconfig "${config_file}" --context "${cluster_context}" apply -f "${dir}/../deploy/argocd/bootstrap/root-app.yaml"
 
